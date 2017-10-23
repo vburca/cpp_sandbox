73f9c622609c26a3e8f424175861ca70f3faf5e4
Vlad Burca <vladburca2005@gmail.com>
2017-10-23 02:18:53 -0700

    Add some sorting playground

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Array: " << endl;
+    print_array(arr, n);
+    bubble_sort(arr, n);
+    cout << "Sorted array: " << endl;
+    print_array(arr, n);
+    return 0;
+}
